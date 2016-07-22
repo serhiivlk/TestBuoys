cc34cc33b6f706625bccccab4f3d95d7ef0e0a51
sergii <skit078@gmail.com>
2016-07-22 16:04:52 +0300

    add arrow

--- app/src/main/java/com/thermsx/localbuoys/adapter/ItemListCursorAdapter.java
+++ app/src/main/java/com/thermsx/localbuoys/adapter/ItemListCursorAdapter.java
@@ -2,12 +2,14 @@ package com.thermsx.localbuoys.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.thermsx.localbuoys.R;
+import com.thermsx.localbuoys.databinding.ListItemBrowseBinding;
 import com.thermsx.localbuoys.model.Item;
 import com.thermsx.localbuoys.provider.table.BrowseContract;
 
@@ -20,23 +22,23 @@ public class ItemListCursorAdapter extends CursorRecyclerViewAdapter<ItemListCur
     @Override
     public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(android.R.layout.simple_list_item_1, parent, false);
+                .inflate(R.layout.list_item_browse, parent, false);
         return new ItemViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ItemViewHolder holder, Cursor cursor) {
         Item item = BrowseContract.fromCursor(cursor);
-        holder.text.setText(item.getName());
+        holder.binding.setItem(item);
     }
 
     public class ItemViewHolder extends RecyclerView.ViewHolder {
-        private TextView text;
+        ListItemBrowseBinding binding;
 
         public ItemViewHolder(View itemView) {
             super(itemView);
 
-            text = (TextView) itemView.findViewById(android.R.id.text1);
+            binding = DataBindingUtil.bind(itemView);
         }
     }
 }

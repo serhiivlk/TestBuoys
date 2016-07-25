84b5cbffafbc2f9df9b028799efb96b5645d6618
sergii <skit078@gmail.com>
2016-07-25 18:38:23 +0300

    tides chart

--- app/src/main/java/com/thermsx/localbuoys/ui/fragment/info/TidesFragment.java
+++ app/src/main/java/com/thermsx/localbuoys/ui/fragment/info/TidesFragment.java
@@ -1,6 +1,7 @@
 package com.thermsx.localbuoys.ui.fragment.info;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -14,8 +15,16 @@ import com.thermsx.localbuoys.api.LocalBuoyService;
 import com.thermsx.localbuoys.api.response.TidesDataResponse;
 import com.thermsx.localbuoys.api.response.TidesGeneralInfoResponse;
 import com.thermsx.localbuoys.databinding.FragmentTidesInfoBinding;
+import com.thermsx.localbuoys.model.TideData;
 import com.thermsx.localbuoys.model.TidesInfo;
+import com.thermsx.localbuoys.model.TidesReturnValue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -67,7 +76,21 @@ public class TidesFragment extends InfoFragment {
         call.enqueue(new Callback<TidesDataResponse>() {
             @Override
             public void onResponse(Call<TidesDataResponse> call, Response<TidesDataResponse> response) {
+                TidesReturnValue returnValue = response.body().getReturnValue();
+                List<TideData> datas = returnValue.getTideDatas();
+                List<PointValue> values = new ArrayList<>();
+                for (TideData data : datas) {
+                    values.add(new PointValue((float) data.getDay(), (float) data.getValue()));
+                }
+                Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+                List<Line> lines = new ArrayList<>();
+                lines.add(line);
 
+                LineChartData data = new LineChartData();
+                data.setLines(lines);
+
+                mBinding.chart.setInteractive(false);
+                mBinding.chart.setLineChartData(data);
             }
 
             @Override
